a39a7f4f1f978e738054b69b6885d4b2da0e8594
Caleb Briancesco <jcaleb4@hotmail.com>
2016-01-06 13:06:57 -0600

    updating

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
 
-gulp.task('default', ['browser-sync', 'stylus', 'jade', 'scripts', 'fonts', 'thirdpartyJs', 'thirdpartyCss', 'data', 'root', 'images', 'watch']);
+gulp.task('default', ['browser-sync', 'stylus', 'jade', 'fonts', 'thirdpartyJs', 'thirdpartyCss', 'data', 'root', 'images', 'scripts', 'watch']);
 
-gulp.task('build', ['stylus-min', 'jade-min', 'scripts-min', 'fonts', 'thirdpartyJs', 'thirdpartyCss', 'data', 'root', 'images']);
+gulp.task('build', ['stylus-min', 'jade-min', 'fonts', 'thirdpartyJs', 'thirdpartyCss', 'data', 'root', 'images', 'scripts-min']);
